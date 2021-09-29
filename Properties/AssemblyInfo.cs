4a9aad5d2023c6671de1086576ce0d3ad8fb9ba9
dvingerh <dirk.ving@gmail.com>
2021-09-29 22:44:55 +0200

    Bump version to 3.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("tb-vol-scroll")]
-[assembly: AssemblyDescription("Tool to enable volume control when your mouse cursor is positioned above the taskbar.")]
+[assembly: AssemblyDescription("Simple utility to enable volume control when your mouse cursor is positioned above the taskbar, among a few other options.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dirk Vingerhoeds")]
 [assembly: AssemblyProduct("tb-vol-scroll")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3")]
-[assembly: AssemblyFileVersion("3.3")]
+[assembly: AssemblyVersion("3.4")]
+[assembly: AssemblyFileVersion("3.4")]
